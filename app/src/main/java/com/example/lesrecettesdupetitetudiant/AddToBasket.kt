0e8e7cfc92e824bddb7ed1fd360fbb96293e8e88
Arthur <62658844+OdArthur@users.noreply.github.com>
2023-03-29 21:47:26 -0400

    fix : AddToBasket.kt

--- app/src/main/java/com/example/lesrecettesdupetitetudiant/AddToBasket.kt
+++ app/src/main/java/com/example/lesrecettesdupetitetudiant/AddToBasket.kt
@@ -1,18 +1,8 @@
 package com.example.lesrecettesdupetitetudiant
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.AdapterView
-import android.widget.AdapterView.OnItemClickListener
-import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.EditText
-import com.example.lesrecettesdupetitetudiant.databinding.ActivityMainBinding
-import com.google.android.material.snackbar.Snackbar
 import com.example.lesrecettesdupetitetudiant.databinding.ActivityAddToBasketBinding
 
 class AddToBasket : AppCompatActivity(){
