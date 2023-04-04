c8a61a39e7da48b2b454f3a87f1c4d291b1ddb2d
Arthur <62658844+OdArthur@users.noreply.github.com>
2023-04-03 21:58:02 -0400

    feat return to main activity after adding a recipe

--- app/src/main/java/com/example/lesrecettesdupetitetudiant/ListRecipeIngredientQuantity.kt
+++ app/src/main/java/com/example/lesrecettesdupetitetudiant/ListRecipeIngredientQuantity.kt
@@ -1,5 +1,6 @@
 package com.example.lesrecettesdupetitetudiant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -63,6 +64,8 @@ class ListRecipeIngredientQuantity : AppCompatActivity() {
             view->
             val IngredientQuant = adapter.GetQuant()
             db.addRecipe(RecipeTitle, RecipeDescription)
+            intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
 
     }
